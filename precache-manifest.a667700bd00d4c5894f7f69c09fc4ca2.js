c633c57426df1720198ff7edfc9f966dedf4880f
Denis <randomlycoding@gmail.com>
2019-04-24 12:35:18 +0400

    Updates

---
+++ precache-manifest.a667700bd00d4c5894f7f69c09fc4ca2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ec040203087389a8e889808cd6bbfc",
+    "url": "/instReactSaver/index.html"
+  },
+  {
+    "revision": "e0efc4bbb8495084db79",
+    "url": "/instReactSaver/static/css/main.953c3cf9.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/instReactSaver/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "e0efc4bbb8495084db79",
+    "url": "/instReactSaver/static/js/main.32fc2cfb.chunk.js"
+  },
+  {
+    "revision": "b8ce9159910752d2993b",
+    "url": "/instReactSaver/static/js/runtime~main.98ca2c2d.js"
+  }
+]);
